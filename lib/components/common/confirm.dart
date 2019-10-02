045c123d5e8ff3b7d7d228f9c275185acbe2f020
Nyatella Toast <psyanite@gmail.com>
2019-10-02 19:01:13 +1000

    Refactor confirm.dart

--- lib/components/common/confirm.dart
+++ lib/components/common/confirm.dart
@@ -13,47 +13,60 @@ class Confirm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var content = Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(height: 10.0),
+        Column(children: <Widget>[
+          Text(title, style: TextStyle(fontSize: 18.0, fontWeight: Burnt.fontBold)),
+          if (description != null) _description(),
+        ]),
+        Container(height: 10.0),
+        _options(context),
+      ],
+    );
+    return AlertDialog(
+      contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      content: Container(
+        width: 100,
+        child: content,
+      ),
+    );
+  }
+
+  Widget _description() {
+    return Container(
+      margin: EdgeInsets.only(top: 5.0),
+      width: 200.0,
+      child: Text(description, textAlign: TextAlign.center, style: TextStyle(color: Color(0xAA604B41), fontSize: 14.0)),
+    );
+  }
+
+  Widget _options(context) {
     var options = <DialogOption>[
       DialogOption(display: action, onTap: onTap, style: TextStyle(color: Color(0xFF51A4FF), fontSize: 16.0, fontWeight: Burnt.fontBold)),
       DialogOption(display: 'Cancel', onTap: () => Navigator.of(context, rootNavigator: true).pop(true), style: TextStyle(fontSize: 16.0)),
     ];
-    return AlertDialog(
-        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-        content: Container(
-            width: 100,
-            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-              Container(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Column(children: <Widget>[
-                    Text(title, style: TextStyle(fontSize: 18.0, fontWeight: Burnt.fontBold)),
-                    if (description != null) Container(height: 5.0),
-                    if (description != null) Container(width: 200.0, child: Text(description, textAlign: TextAlign.center, style: TextStyle(color: Color(0xAA604B41), fontSize: 14.0))),
-                  ])),
-              Container(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: List<Widget>.from(options.map((o) => Column(children: <Widget>[
-                        Divider(
-                          color: Color(0x16007AFF),
-                        ),
-                        _option(o)
-                      ]))),
-                ),
-              ),
-            ])));
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: List<Widget>.from(options.map((o) {
+        return Column(children: <Widget>[Divider(color: Color(0x16007AFF)), _option(o)]);
+      })),
+    );
   }
 
   Widget _option(DialogOption option) {
     return InkWell(
-        child: Container(
-          height: 40.0,
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[Text(option.display, style: option.style)],
-          ),
+      onTap: option.onTap,
+      child: Container(
+        height: 40.0,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[Text(option.display, style: option.style)],
         ),
-        onTap: option.onTap);
+      ),
+    );
   }
 }
